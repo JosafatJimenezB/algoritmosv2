6f42310ff28c16b56e4aec92cd4e7c492a0c0ee6
josafat <josa221001@gmail.com>
2022-11-03 10:42:19 -0600

    add: agregando recursividad

---
+++ src/com/example/recursividad/main.java
@@ -0,0 +1,47 @@
+package com.example.recursividad;
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+        /// Declaracion de los arreglos y variables
+        int opc = 0;
+        int[] a = {2, 3, 8, 1, 9};
+
+        //Declaracion de los objetos
+        recurLogic ar = new recurLogic(a);
+        Scanner sc = new Scanner(System.in);
+
+        do{
+            System.out.println("--------- Menu ---------");
+            System.out.println("1.- Imprimir Arreglo");
+            System.out.println("2.- Buscar en el arreglo");
+            System.out.println("3.- Imprimir Matriz");
+            System.out.println("4.- Salir....");
+            opc = sc.nextInt();
+
+            switch (opc){
+                case 1:
+                    System.out.println("Impresion del arreglo");
+                    ar.printArray(0);
+                    break;
+
+                case 2:
+                    System.out.println("Buscar dentro del arreglo");
+                    System.out.println("Ingrese el numero a encontrar");
+                    int b = sc.nextInt();
+                    ar.recursiveSearch(a, b);
+                    break;
+
+                case 3:
+                    System.out.println("Impresion de la matriz");
+                    break;
+
+                case 4:
+                    System.out.println("Saliendo...");
+            }
+
+
+        }while(opc != 4);
+    }
+}
---
+++ src/com/example/recursividad/recurLogic.java
@@ -0,0 +1,37 @@
+package com.example.recursividad;
+
+public class recurLogic {
+
+    static int pos = 0;
+    private int[] vector;
+    public recurLogic(int[] v) {
+        vector = v;
+    }
+
+    public void printArray(int pos){
+        if (pos == vector.length-1) {
+            System.out.println("Elemento en la posición "+pos + " es "+vector[pos]);
+        }
+        else {
+            System.out.println("Elemento en la posición "+pos + " es "+vector[pos]);
+            printArray(pos+1);
+        }
+    }
+
+    public static int recursiveSearch(int[] arr, int numtoSearch) {
+        if (pos>=arr.length) {
+            pos=0;
+            return -1;
+        }
+        if (arr[pos]==numtoSearch){
+            System.out.println("el numero " + numtoSearch + " se encuentra en la posicion " +pos);
+            return (pos+1);
+        }
+        else {
+            pos++;
+            return recursiveSearch(arr, numtoSearch);
+        }
+
+    }
+
+}
